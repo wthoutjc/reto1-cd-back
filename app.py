8a20a19df06517573bfcd258d9bfd78c53c051c6
wthoutjc <juancamilorr2012@hotmail.com>
2022-06-04 10:45:48 -0500

    backend beta

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app.config['JWT_SECRET_KEY'] = app.config['SECRET_KEY']
 
 @app.route("/", methods=['GET', 'POST'])
 def root_data():
+    dataset = Dataset("www.datos.gov.co", "gt2j-8ykr")
     if request.method == 'POST':
         data_raw = request.data.decode("utf-8")
         json_data = json.loads(data_raw)
@@ -17,13 +18,15 @@ def root_data():
         filtro = json_data['filtro']
         valor = json_data['valor']
 
+        filtro = dataset.normalize_column(filtro)
+        valor = dataset.normalize_value(filtro, valor)
+
         # result es el nuevo df dado un filtro y un valor
-        result = 0
+        result = dataset.query(filtro, valor)
 
         return make_response(jsonify(result), 200)
-    dataset = Dataset("www.datos.gov.co", "gt2j-8ykr")
-    df = dataset.__getitem__()
-    return make_response(jsonify(df), 200)
+    records = dataset.__getitem__()
+    return make_response(jsonify(records), 200)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
--- model/data.py
+++ model/data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from unicodedata import normalize
 # Path: model\data.py
 # Data extraction
 from sodapy import Socrata
@@ -11,8 +11,42 @@ class Dataset:
         self.client = Socrata(self.url_connect, None)
         try:
             self.results = self.client.get(self.dataset_id, limit=100)
+            self.df = pd.DataFrame(self.results)
+            self.df['edad'] = self.df['edad'].apply(int)
         except:
             self.results = None
+            self.df = None
     
     def __getitem__(self):
-        return pd.DataFrame(self.results).to_dict('records')
+        return self.df.to_dict('records')
+
+    def query(self, column, value):
+        if type(value) == list:
+            query = self.df.query(f"{column} > {value[0]} and {column} < {value[1]}")
+            return query.to_dict('records')
+
+        query = self.df.query(f"{column} == '{value}'")
+        return query.to_dict('records')
+
+    def normalize_column(self, column: str):
+        unicode = dict.fromkeys(map(ord, u'\u0301'))
+        name = normalize('NFKC', normalize('NFKD', column).translate(unicode))
+        name = name.replace(' ', '_')
+        name = name.replace('_de_', '_')
+        name = name.lower()
+
+        return name
+
+    def normalize_value(self, column: str, value: str):
+        if column == 'edad':
+            value = value.replace('[', '').replace(']', '').replace(' ', '')
+            rango = [int(x) for x in value.split(',')]
+            return rango
+
+        if column == 'sexo':
+            value = value[0:1]
+
+        return value
+
+
+    
