20c2b887feb25e6857443769858f1dd7bf9813e1
wthoutjc <juancamilorr2012@hotmail.com>
2022-06-04 09:38:50 -0500

    endpoint preparado

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, make_response, jsonify
-
+from flask import Flask, make_response, jsonify, request
+import json
 # Datas
 from model.data import Dataset
 
@@ -8,8 +8,19 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'UHGx14#&17NoPRQS#12'
 app.config['JWT_SECRET_KEY'] = app.config['SECRET_KEY']
 
-@app.route("/")
-def hello_world():
+@app.route("/", methods=['GET', 'POST'])
+def root_data():
+    if request.method == 'POST':
+        data_raw = request.data.decode("utf-8")
+        json_data = json.loads(data_raw)
+
+        filtro = json_data['filtro']
+        valor = json_data['valor']
+
+        # result es el nuevo df dado un filtro y un valor
+        result = 0
+
+        return make_response(jsonify(result), 200)
     dataset = Dataset("www.datos.gov.co", "gt2j-8ykr")
     df = dataset.__getitem__()
     return make_response(jsonify(df), 200)
